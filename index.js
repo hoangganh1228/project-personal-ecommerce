771724e0d2f71e843f67032b43f7291c448bf341
Hoàng Anh. Nguyễn <hoanganh52521352@gmail.com>
2023-12-13 15:36:33 +0700

    câu hình MVC

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+
+
+const route = require("./routes/client/index.route");
+
+app.set("views", "./views");
+app.set("view engine", "pug");
+
+
+// Routes
+route(app);
+
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+})
