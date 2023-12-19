99dcf004506d9a9d0ea052efc2e46646034cdc5c
Hoàng Anh. Nguyễn <hoanganh52521352@gmail.com>
2023-12-19 22:14:21 +0700

    Hoàn thiện phần sản phẩm

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,6 +1,16 @@
+const Product = require("../../models/product.model")
+
 module.exports.index = async (req, res) => {
+    const products = await Product.find({
+        status: "active"
+    })
+
+    // console.log(products);
+
+
+
     res.render("client/pages/products/index", {
-        pageTitle: "Trang sản phẩm"
-        
+        pageTitle: "Trang sản phẩm",
+        products: products
     })
 }
