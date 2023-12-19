d0f2344cddf31ed9e8772c53dc09c26048a123f8
Hoàng Anh. Nguyễn <hoanganh52521352@gmail.com>
2023-12-19 22:24:54 +0700

    Làm trang danh sách sản phẩm bên Client

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -2,15 +2,20 @@ const Product = require("../../models/product.model")
 
 module.exports.index = async (req, res) => {
     const products = await Product.find({
-        status: "active"
+        status: "active",
+        deleted: false
     })
 
     // console.log(products);
-
+    
+    const newProducts = products.map(item => {
+        item.priceNew = (item.price-(item.price*item.discountPercentage)/100).toFixed(0);
+        return item;
+    });
 
 
     res.render("client/pages/products/index", {
         pageTitle: "Trang sản phẩm",
-        products: products
+        products: newProducts
     })
 }
