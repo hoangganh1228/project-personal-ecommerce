fabbdc916ba6243f9234a085a3fcdec968476798
Hoàng Anh. Nguyễn <hoanganh52521352@gmail.com>
2024-01-12 17:59:22 +0700

    1.2. Tối ưu phần Tìm kiếm

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,7 +1,7 @@
 const Product = require("../../models/product.model")
 
 const filterStatusHelper = require("../../helpers/filterStatus")
-
+const searchHelper = require("../../helpers/search")
 
 // [GET] /admin/products
 module.exports.index = async (req, res) => {
@@ -20,12 +20,10 @@ module.exports.index = async (req, res) => {
         find.status = req.query.status;
     }
 
-    let keyword = "";
-    if(req.query.keyword) {
-        keyword = req.query.keyword;
-
-        const regex = new RegExp(keyword, "i")
-        find.title = regex;
+    // Tìm kiếm
+    const objectSearch = searchHelper(req.query);
+    if(objectSearch.regex) {
+        find.title = objectSearch.regex
     }
 
     const products = await Product.find(find);
@@ -36,6 +34,6 @@ module.exports.index = async (req, res) => {
         pageTitle: "Trang sản phẩm",
         products: products,
         filterStatus: filterStatus,
-        keyword: keyword
+        keyword: objectSearch.keyword 
     })
 }   
