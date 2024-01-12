be260f6d51e5fd301226ba67586517fcb1dc4b99
Hoàng Anh. Nguyễn <hoanganh52521352@gmail.com>
2024-01-12 23:41:33 +0700

    2. Tính năng phân trang

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -26,7 +26,27 @@ module.exports.index = async (req, res) => {
         find.title = objectSearch.regex
     }
 
-    const products = await Product.find(find);
+    const countProducts = await Product.countDocuments(find);
+
+    // Pagination
+    let objectPagination = {
+        currentPage: 1,
+        limitItems: 4
+    }
+
+    if(req.query.page) {
+        objectPagination.currentPage = parseInt(req.query.page);
+    }
+    objectPagination.skip = (objectPagination.currentPage - 1) * objectPagination.limitItems; 
+    
+    const totalPage = Math.ceil(countProducts/objectPagination.limitItems);
+    // console.log(totalPage);
+    objectPagination.totalPage = totalPage;
+    // End Pagination
+
+    
+
+    const products = await Product.find(find).limit(objectPagination.limitItems).skip(objectPagination.skip);
 
     // console.log(products);
 
@@ -34,6 +54,7 @@ module.exports.index = async (req, res) => {
         pageTitle: "Trang sản phẩm",
         products: products,
         filterStatus: filterStatus,
-        keyword: objectSearch.keyword 
+        keyword: objectSearch.keyword,
+        pagination: objectPagination
     })
 }   
