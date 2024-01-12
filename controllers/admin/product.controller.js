caf726d4122c720068ea2be115676c33893335f9
Hoàng Anh. Nguyễn <hoanganh52521352@gmail.com>
2024-01-12 17:12:56 +0700

    4. Tính năng tìm kiếm (Phần 2)

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -39,6 +39,14 @@ module.exports.index = async (req, res) => {
         find.status = req.query.status;
     }
 
+    let keyword = "";
+    if(req.query.keyword) {
+        keyword = req.query.keyword;
+
+        const regex = new RegExp(keyword, "i")
+        find.title = regex;
+    }
+
     const products = await Product.find(find);
 
     // console.log(products);
@@ -46,6 +54,7 @@ module.exports.index = async (req, res) => {
     res.render("admin/pages/products/index", {
         pageTitle: "Trang sản phẩm",
         products: products,
-        filterStatus: filterStatus
+        filterStatus: filterStatus,
+        keyword: keyword
     })
 }   
