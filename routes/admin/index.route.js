ea28fecd2be0a3794e9b61b5e85e38f8d291e085
Hoàng Anh. Nguyễn <hoanganh52521352@gmail.com>
2024-01-30 21:03:05 +0700

    2. Làm tính năng đăng nhập

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -10,6 +10,8 @@ const roleRoutes = require("./role.route");
 
 const accountRoutes = require("./account.route");
 
+const authRoutes = require("./auth.route");
+
 
 module.exports = (app) => {
     const PATH_ADMIN = systemConfig.prefixAdmin;
@@ -23,5 +25,7 @@ module.exports = (app) => {
     app.use(PATH_ADMIN + "/roles", roleRoutes);
 
     app.use(PATH_ADMIN + "/accounts", accountRoutes);
+
+    app.use(PATH_ADMIN + "/auth", authRoutes);
 
 }   
