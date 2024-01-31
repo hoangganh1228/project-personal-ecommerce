c2eb2b36140e23476d0c7701aba6a3649675c271
Hoàng Anh. Nguyễn <hoanganh52521352@gmail.com>
2024-01-31 22:37:33 +0700

    1. Thêm authorization

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -36,10 +36,18 @@ module.exports = (app) => {
         productCategoryRoutes
     );
 
-    app.use(PATH_ADMIN + "/roles", roleRoutes);
+    app.use(
+        PATH_ADMIN + "/roles", 
+        authMiddleware.requireAuth,
+        roleRoutes
+    );
 
-    app.use(PATH_ADMIN + "/accounts", accountRoutes);
+    app.use(
+        PATH_ADMIN + "/accounts",
+        authMiddleware.requireAuth,
+        accountRoutes
+    );
 
     app.use(PATH_ADMIN + "/auth", authRoutes);
 
-}   
+}       
