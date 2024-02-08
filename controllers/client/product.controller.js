7234b2721ac7b3a5748d46d4ef9ed7142b7f5769
Hoàng Anh. Nguyễn <hoanganh52521352@gmail.com>
2024-02-08 10:19:57 +0700

    5. Trang danh sách sản phẩm bên client

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,8 +1,9 @@
 const Product = require("../../models/product.model")
+const ProductCategory = require("../../models/product-category.model");
 
 const createTreeHelper = require("../../helpers/createTree");   
 const productHelper = require("../../helpers/product")
-
+const productsCategoryHelper = require("../../helpers/products-category");
 // [GET] /products
 module.exports.index = async (req, res) => {
     const products = await Product.find({
@@ -47,5 +48,38 @@ module.exports.detail = async (req, res) => {
 
  
 }
+
+// [GET] /detail/router.get("/:slugCategory", controller.category);
+module.exports.category = async (req, res) => {
+    try {
+        const category = await ProductCategory.findOne({
+            slug: req.params.slugCategory,
+            deleted: false,
+            status: "active"
+    
+        })
+
+        const listSubCategory = await productsCategoryHelper.getSubCategory(category.id);
+
+  const listSubCategoryId = listSubCategory.map(item => item.id);
+
+
+        const products = await Product.find({
+            product_category_id: { $in: [category.id, ...listSubCategoryId] },
+            deleted: false
+        }).sort({ positipn: "desc" });
+        
+        const newProducts = productHelper.priceNewProducts(products);
+        
+        res.render("client/pages/products/index", {
+            pageTitle: category.title,
+            products: newProducts,
+        })
+    } catch (error) {
+        res.redirect(`/products`)
+    }
+    
+}
+
 
 
