3a608b8e5af2f4b8e1ec2a5013cc577acbc2f16a
Hoàng Anh. Nguyễn <hoanganh52521352@gmail.com>
2024-02-08 21:19:15 +0700

    6. Trang chi tiết sản phẩm bên client

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -2,7 +2,7 @@ const Product = require("../../models/product.model")
 const ProductCategory = require("../../models/product-category.model");
 
 const createTreeHelper = require("../../helpers/createTree");   
-const productHelper = require("../../helpers/product")
+const productsHelper = require("../../helpers/product")
 const productsCategoryHelper = require("../../helpers/products-category");
 // [GET] /products
 module.exports.index = async (req, res) => {
@@ -14,8 +14,7 @@ module.exports.index = async (req, res) => {
     
     // console.log(products);
     
-    const newProducts = productHelper.priceNewProducts(products);
-
+    const newProducts = productsHelper.priceNewProducts(products);
 
     res.render("client/pages/products/index", {
         pageTitle: "Trang sản phẩm",
@@ -30,12 +29,25 @@ module.exports.detail = async (req, res) => {
     try {
         const find = {
             deleted: false,
-            slug: req.params.slug
+            slug: req.params.slugProduct,
         }
     
         const product = await Product.findOne(find)
         
-    
+
+        if(product.product_category_id) {
+            const category = await ProductCategory.findOne({
+              _id: product.product_category_id,
+              status: "active",
+              deleted: false
+            });
+  
+            product.category = category;
+          }
+          
+
+          product.priceNew = productsHelper.priceNewProduct(product);
+
         res.render("client/pages/products/detail", {
             pageTitle: product.title,
             product: product
@@ -61,15 +73,15 @@ module.exports.category = async (req, res) => {
 
         const listSubCategory = await productsCategoryHelper.getSubCategory(category.id);
 
-  const listSubCategoryId = listSubCategory.map(item => item.id);
+        const listSubCategoryId = listSubCategory.map(item => item.id);
 
 
         const products = await Product.find({
             product_category_id: { $in: [category.id, ...listSubCategoryId] },
             deleted: false
-        }).sort({ positipn: "desc" });
+        }).sort({ position: "desc" });
         
-        const newProducts = productHelper.priceNewProducts(products);
+        const newProducts = productsHelper.priceNewProducts(products);
         
         res.render("client/pages/products/index", {
             pageTitle: category.title,
