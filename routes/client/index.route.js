2d7077ad4636e79410a9c43ba51761c427a282c6
Hoàng Anh. Nguyễn <hoanganh52521352@gmail.com>
2024-02-11 16:29:30 +0700

    2.1. Thêm sản phẩm vào giỏ hàng

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,16 +1,22 @@
 const categoryMiddleware =  require("../../middlewares/client/category.middleware");
+const cartMiddleware =  require("../../middlewares/client/cart.middleware");
 
 const homeRoutes = require("./home.route");
 const productRocutes = require("./product.route");
 const searchRoutes = require("./search.route");
+const cartRoutes = require("./cart.route");
 
 
 module.exports = (app) => {
-    app.use(categoryMiddleware.category)
-
+    app.use(categoryMiddleware.category);
+    app.use(cartMiddleware.cartId);
+    
     app.use("/", homeRoutes)
 
     app.use("/products", productRocutes)
 
     app.use("/search", searchRoutes)
+
+    app.use("/cart", cartRoutes)
+
 }
