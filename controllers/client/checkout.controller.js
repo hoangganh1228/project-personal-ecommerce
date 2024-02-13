55149bcb0ab3f08fad9a3f0c4db30f55288032e7
Hoàng Anh. Nguyễn <hoanganh52521352@gmail.com>
2024-02-13 09:05:36 +0700

    1.2 + 1.3 + 1.4 ( tiếp )

--- controllers/client/checkout.controller.js
+++ controllers/client/checkout.controller.js
@@ -95,7 +95,31 @@ module.exports.order = async (req, res) => {
 module.exports.success = async (req, res) => {
     // console.log(req.params.orderId);
 
+    const order = await Order.findOne({
+        _id: req.params.orderId
+    })
+
+    // console.log(order);
+
+    for(const product of order.products) {
+        const productInfo = await Product.findOne({
+            _id: product.product_id
+        }).select("title thumbnail")
+        
+        // console.log(productInfo);
+        product.productInfo = productInfo;
+
+        product.priceNew = productsHelper.priceNewProduct(product);
+
+        product.totalPrice = product.priceNew * product.quantity
+    }
+
+    order.totalPrice = order.products.reduce((sum, item) => sum + item.totalPrice, 0)
+
+
     res.render("client/pages/checkout/success", {
       pageTitle: "Đặt hàng thành công",
+      order: order
+    
     });
 }
