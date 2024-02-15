ff2501e8288ad29521b7820dfcb7b2a5e18c6111
Hoàng Anh. Nguyễn <hoanganh52521352@gmail.com>
2024-02-15 16:21:45 +0700

    3. Quên mật khẩu (Phần 2)

--- routes/client/user.route.js
+++ routes/client/user.route.js
@@ -44,4 +44,25 @@ router.post(
 
 );
 
+router.get(
+    "/password/otp",
+    controller.otpPassword,
+);
+
+router.post(
+    "/password/otp",
+    controller.otpPasswordPost,
+);
+
+router.get(
+    "/password/reset",
+    controller.resetPassword,
+);
+
+router.post(
+    "/password/reset",
+    validate.resetPasswordPost,
+    controller.resetPasswordPost,
+);
+
 module.exports = router;
--- validates/client/user.validate.js
+++ validates/client/user.validate.js
@@ -46,3 +46,25 @@ module.exports.forgotPasswordPost = (req, res, next) => {
 
   next();
 }
+
+module.exports.resetPasswordPost = (req, res, next) => {
+  if(!req.body.password) {
+    req.flash("error", `Vui lòng nhập mật khẩu!`);
+    res.redirect("back");
+    return;
+  }
+
+  if(!req.body.confirmPassword) {
+    req.flash("error", `Vui lòng xác nhận mật khẩu!`);
+    res.redirect("back");
+    return;
+  }
+
+  if(req.body.confirmPassword != req.body.password) {
+    req.flash("error", `Mật khẩu không khớp!`);
+    res.redirect("back");
+    return;
+  }
+
+  next();
+}
