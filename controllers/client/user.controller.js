f439308ea29c357f7faaaf1561ca13caea403dc5
Hoàng Anh. Nguyễn <hoanganh52521352@gmail.com>
2024-02-17 15:49:12 +0700

    Khi đăng nhập cập nhật cartId

--- controllers/client/user.controller.js
+++ controllers/client/user.controller.js
@@ -76,14 +76,25 @@ module.exports.loginPost = async (req, res) => {
         return;
     }
 
+    const cart = await Cart.findOne({
+        user_id: user.id    
+    })
+
+    if(cart) {
+        res.cookie("cartId", cart.id)
+    } else {
+        await Cart.updateOne({
+            _id: req.cookies.cartId
+            }, {
+            user_id: user.id
+        });
+    }
+
+
     // console.log(req.cookies.cartId);
     // console.log(user.id);
 
-    await Cart.updateOne({
-        _id: req.cookies.cartId
-        }, {
-        user_id: user.id
-    });
+    
 
     res.cookie("tokenUser", user.tokenUser)
 
@@ -93,6 +104,7 @@ module.exports.loginPost = async (req, res) => {
 // [GET] /user/logout
 module.exports.logout = async (req, res) => {
     res.clearCookie("tokenUser");
+    res.clearCookie("cartId");
     res.redirect("/");
 }
 
