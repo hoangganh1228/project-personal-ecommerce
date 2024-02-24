bfe6f21d20f5b5c5d6cac74aba4e1180568ad921
Hoàng Anh. Nguyễn <hoanganh52521352@gmail.com>
2024-02-24 17:41:17 +0700

    Làm phần thông báo và đăng nhập

--- middlewares/client/cart.middleware.js
+++ middlewares/client/cart.middleware.js
@@ -1,4 +1,7 @@
 const Cart = require("../../models/cart.model")
+const Product = require("../../models/product.model");
+
+const productsHelper = require("../../helpers/product");
 
 module.exports.cartId = async (req, res, next) => {
     if(!req.cookies.cartId) {
@@ -16,6 +19,25 @@ module.exports.cartId = async (req, res, next) => {
         const cart = await Cart.findOne({
             _id: req.cookies.cartId
         })
+
+        if (cart.products.length > 0) {
+            for (const item of cart.products) {
+                const productId  = item.product_id;
+                const productInfo = await Product.findOne({
+                    _id: productId,
+                }).select("title thumbnail slug price discountPercentage");
+    
+                productInfo.priceNew = productsHelper.priceNewProduct(productInfo);
+    
+                item.productInfo = productInfo;
+    
+                item.totalPrice = productInfo.priceNew * item.quantity;
+    
+                cart.totalPrice = cart.products.reduce((sum, item) => sum + item.totalPrice, 0);
+            }
+        }
+    
+
         cart.totalQuantity = cart.products.reduce((sum, item) => sum + item.quantity, 0);
 
         res.locals.miniCart = cart;
